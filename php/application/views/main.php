36786de16971b2867f68df42d0a5c64ce8fe0d11
Brian Li <brianliwaichun@gmail.com>
2015-01-09 01:34:19 +0800

    hh

---
+++ php/application/views/main.php
@@ -0,0 +1,31 @@
+<link rel="stylesheet" type="text/css" href="../../bootstrap-3.1.1-dist/css/bootstrap.css">
+<script src="../../bootstrap-3.1.1-dist/js/bootstrap.js"></script>
+<script src="../../bootstrap-3.1.1-dist/js/jquery-2.1.0.js"></script>
+<script src="../../bootstrap-3.1.1-dist/js/all.js"></script>
+ <nav class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="main">Cloud Based Multiple Drawing System</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li><a href="register">User</a></li>
+        <li><a href="member_register">Member</a></li>
+		<li><a href="product_register">Product</a></li>
+		<li><a href="Cashout">Review</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="login">Logout</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+ </nav>
